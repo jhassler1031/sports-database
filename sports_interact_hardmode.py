b122ea36611bf29016e646561926f3b930789a51
jhassler1031 <jhassler1031@gmail.com>
2018-05-29 15:52:25 -0400

    added option to change a value for a given team, setup options for changing each of the 4 values, reprint after making a change

--- sports_interact_hardmode.py
+++ sports_interact_hardmode.py
@@ -8,9 +8,10 @@ while True:
 What would you like to do?
 Enter 1 to search for a team
 Enter 2 to add a new team to the database
-Enter 3 to show top performers by a certain stat:
+Enter 3 to show top performers by a certain stat
+Enter 4 to adjust stats for a certain team:
 """)
-    if user_choice == "1" or user_choice == "2" or user_choice == "3":
+    if user_choice == "1" or user_choice == "2" or user_choice == "3" or user_choice == "4":
         break
     else:
         print("Invalid input.")
@@ -49,3 +50,46 @@ Enter 2 for points:
         team_sort = db.query("SELECT * FROM sports_stats ORDER BY points DESC;")
         for row in team_sort[:5]:
             print(row)
+elif user_choice == "4":
+    team_name = input("Enter team name whose stats you wish to edit: ")
+    #print the team values so you can see what you want to change
+    team_data = db.query("SELECT * FROM sports_stats WHERE team_name=:team;", team=team_name)
+    for row in team_data:
+        print(row)
+    while True:
+        stat_to_adjust = input("""
+Select which value to adjust,
+Enter 1 for team name
+Enter 2 for wins
+Enter 3 for losses
+Enter 4 for points
+""")
+        if stat_to_adjust == "1" or stat_to_adjust == "2" or stat_to_adjust == "3" or stat_to_adjust =="4":
+            break
+        else:
+            print("Invalid input.")
+    if stat_to_adjust == "1":
+        adjustment = input("Enter new team name: ")
+        db.query("UPDATE sports_stats SET team_name=:name WHERE team_name=:team;",
+        name=adjustment,
+        team=team_name)
+    elif stat_to_adjust == "2":
+        adjustment = input("Enter new number of wins: ")
+        db.query("UPDATE sports_stats SET wins=:num_wins WHERE team_name=:team;",
+        num_wins=adjustment,
+        team=team_name)
+    elif stat_to_adjust == "3":
+        adjustment = input("Enter new number of losses: ")
+        db.query("UPDATE sports_stats SET losses=:num_losses WHERE team_name=:team;",
+        num_losses=adjustment,
+        team=team_name)
+    elif stat_to_adjust == "4":
+        adjustment = input("Enter new number of points: ")
+        db.query("UPDATE sports_stats SET points=:num_points WHERE team_name=:team;",
+        num_points=adjustment,
+        team=team_name)
+
+    #Reprint the team info showing new values
+    team_data = db.query("SELECT * FROM sports_stats WHERE team_name=:team;", team=team_name)
+    for row in team_data:
+        print(row)
