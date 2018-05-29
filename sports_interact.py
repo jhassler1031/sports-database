c04ef35951d070b3d22d1be68358d925aefc78cf
jhassler1031 <jhassler1031@gmail.com>
2018-05-29 15:02:10 -0400

    first user input, allow user to search for a team

--- sports_interact.py
+++ sports_interact.py
@@ -2,3 +2,20 @@
 import records
 
 db = records.Database("postgres://localhost/sports_db")
+
+while True:
+    user_choice = input("""
+What would you like to do?
+Enter 1 to search for a team
+Enter 2 to add a new team to the database:
+""")
+    if user_choice == "1" or user_choice == "2":
+        break
+
+if user_choice == "1":
+    team_name = input("Enter team name: ")
+    team_data = db.query("SELECT * FROM sports_stats WHERE team_name=:team;", team=team_name)
+    for row in team_data:
+        print(row)
+elif user_choice == "2":
+    pass
